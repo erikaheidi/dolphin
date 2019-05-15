7d3f39791d95782cd06f2129b6530524d92532f0
erika <efockereinaldo@digitalocean.com>
2019-05-15 16:42:26 +0200

    Added config sample

---
+++ config_sample.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Default Configuration options
+ */
+
+return $dolphin_config = [
+
+
+    // DigitalOcean API Token
+    'DO_API_TOKEN' => 'YOUR_DIGITALOCEAN_API_TOKEN',
+
+    //Default group
+    'DEFAULT_SERVER_GROUP' => 'servers',
+
+    ///Enables local cache
+    'LOCAL_CACHE_ENABLED' => true,
+
+    //Cache location relative to doc root */
+    'CACHE_DIR' => 'cache',
+
+    //Cache expiry time in minutes
+    'CACHE_EXPIRY' => 60,
+
+];
